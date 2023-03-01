9e2e5f04620c4be6ff9dd791d362c69089582591
hoonjoo94 <hoonjoo94@naver.com>
2023-03-01 13:04:54 +0900

    project init

---
+++ env.example.js
@@ -0,0 +1,4 @@
+export default {
+  devAndroidApiUrl: 'http://192.168.0.1:3010',
+  devIosApiUrl: 'http://localhost:3010',
+};
---
+++ screens/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native/types';
+
+const Home = () => {
+  return <View />;
+};
+
+export default Home;
