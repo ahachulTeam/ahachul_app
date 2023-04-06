88d8e2bf609815c4eb9b78549e5bfbc9c6cf06cc
hoonjoo94 <hoonjoo94@naver.com>
2023-04-07 02:15:34 +0900

    feat: 팝업 모달 공통 컴포넌트 구현 (svg 넣는 기능은 아직 추가되지 않았음)

--- App/stacks/RootStack.tsx
+++ App/stacks/RootStack.tsx
@@ -2,12 +2,18 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import MainStack from './MainStack'
 import AuthStack from './AuthStack'
+import { useRecoilValue } from 'recoil'
+import { isPopupModalVisibleState } from '../../stores/global'
+import PopupModal from '../../components/common/PopupModal'
 
 const RootStack = () => {
   const RootStack = createStackNavigator()
+  const isPopupModalVisible = useRecoilValue(isPopupModalVisibleState)
 
   return (
     <>
+      {isPopupModalVisible && <PopupModal />}
+
       <NavigationContainer>
         <RootStack.Navigator screenOptions={{ headerShown: false }}>
           <RootStack.Screen name="MainStack" component={MainStack} />
