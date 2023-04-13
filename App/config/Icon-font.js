abad9855bf49cd49f64be2a0d50881108f3e2539
Suji Chae <63655826+suji8073@users.noreply.github.com>
2023-04-13 20:04:05 +0900

    fix: fontello Icon 파일 확장자 변경
    ts -> js

--- App/config/Icon-font.js
+++ App/config/Icon-font.js
@@ -3,5 +3,4 @@ import fontelloConfig from './Icon-font.json'
 
 const Icon = createIconSetFromFontello(fontelloConfig, 'fontello')
 const AhIcon = ({ size, ...props }) => <Icon size={size} {...props} />
-
 export default AhIcon
