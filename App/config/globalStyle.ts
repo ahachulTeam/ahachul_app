8259b3a73ecc7eb5c5cf21cc6c67c458b48029d4
Suji Chae <63655826+suji8073@users.noreply.github.com>
2023-04-17 16:16:37 +0900

    chore: globalStyle colors 수정 및 추가

--- App/config/globalStyle.ts
+++ App/config/globalStyle.ts
@@ -1,10 +1,30 @@
-import { Dimensions } from 'react-native'
+import { Dimensions, StyleSheet } from 'react-native'
 
 export const colors = {
+  gray10: '#F2F2F2',
+  gray20: '#E3E3E3',
+  gray30: '#CFCFCF',
+  gray40: '#ADADAD',
+  gray50: '#676767',
+  gray60: '#575757',
+  gray70: '#474747',
+  gray80: '#373737',
   black: '#272727',
-  white: '#fff',
-  gray: '#8b8b8b',
-  blue: '#00baf6',
+
+  white: '#FFF',
+  gray: '#8B8B8B',
+  blue: '#00BAF6',
+  yellow: '#FEE102',
+  red: '#EE4D4D',
+
+  ColorBCBCBC: '#BCBCBC',
+  ColorE2E2E2: '#E2E2E2',
+  ColorCOCOCO: '#COCOCO',
+  ColorE9E9E9: '#E9E9E9',
+  ColorC2C2C2: '#C2C2C2',
+  ColorD0EEFF: '#D0EEFF',
+  ColorDFDFDF: '#DFDFDF',
+  Color8C8C8C: '#8C8C8C',
 }
 
 export const fonts = {
@@ -15,7 +35,23 @@ export const fonts = {
   PretendardVariable: 'PretendardVariable',
 }
 
-export const images = {}
+export const images = {
+  googleIcon: require('../../images/google.svg'),
+  kakaoIcon: require('../../images/kakao.svg'),
+}
+
+export const globalStyle = StyleSheet.create({
+  flexOne: {
+    flex: 1,
+  },
+  flexRowCenter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textAlignCenter: {
+    textAlign: 'center',
+  },
+})
 
 export const screenHeight = Dimensions.get('screen').height
 export const screenWidth = Dimensions.get('screen').width
