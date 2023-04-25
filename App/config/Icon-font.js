ec61d791538f0cefbf8a44213536a2b0d8a863af
Suji Chae <63655826+suji8073@users.noreply.github.com>
2023-04-25 21:32:59 +0900

    delete: fontello 삭제
    - 디자인에서 svg 잘 불러오지 못하는 문제로 인해 사용하지 않기로 함

--- App/config/Icon-font.js
+++
@@ -1,6 +0,0 @@
-import { createIconSetFromFontello } from 'react-native-vector-icons'
-import fontelloConfig from './Icon-font.json'
-
-const Icon = createIconSetFromFontello(fontelloConfig, 'fontello')
-const AhIcon = ({ size, ...props }) => <Icon size={size} {...props} />
-export default AhIcon
