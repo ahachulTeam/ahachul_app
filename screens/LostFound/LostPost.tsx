d43f6ab4e58aa9f6a4b90f1907ae3454bab2ff6c
seilylook <seilylook@naver.com>
2023-05-16 14:01:54 +0900

    feat: lost posts 컴포넌트

--- screens/LostFound/LostPost.tsx
+++ screens/LostFound/LostPost.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, Image } from 'react-native'
+import CSText from '../../components/common/text'
+import CSScreen from '../../components/common/screen'
+import { colors, icons } from '../../App/config/globalStyle'
+import styled from '@emotion/native'
+import { WithLocalSvg } from 'react-native-svg'
 
 type PostProp = {
   post: {
@@ -18,32 +23,81 @@ type PostProp = {
 
 const LostPost = ({ post }: PostProp) => {
   return (
-    <>
-      <View style={styles.item}>
-        <Text style={styles.text}>{post.title}</Text>
+    <CSScreen>
+      <View style={styles.container}>
+        <Image
+          style={styles.tinyImg}
+          source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+        />
+        <View>
+          <CSText weight="700" style={styles.title}>
+            {post.title}
+          </CSText>
+          <CSText weight="400" style={styles.content}>
+            {post.content}
+          </CSText>
+          <View
+            style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <CSText weight="400" style={styles.subwayLine}>
+              {post.subwayLine}호선 | {post.createdBy}분 전
+            </CSText>
+            <CSText weight="400" style={styles.chat}>
+              {post.chats}개의 댓글
+            </CSText>
+          </View>
+        </View>
       </View>
-      <View>
-        <Text style={styles.text}>{post.content}</Text>
-      </View>
-      <View>
-        <Text>{post.chats}</Text>
-      </View>
-    </>
+    </CSScreen>
   )
 }
 
 const styles = StyleSheet.create({
-  item: {
+  container: {
+    marginTop: 10,
+    marginLeft: 10,
+    marginBottom: 10,
     flexDirection: 'row',
-    padding: 16,
-    verticalAlign: 'center',
+    width: 280,
   },
 
-  text: {
-    flex: 1,
+  tinyImg: {
+    marginRight: 5,
+    width: 75,
+    height: 69,
+    borderRadius: 5,
+  },
+
+  title: {
+    verticalAlign: 'top',
     fontSize: 16,
-    color: '#212121',
+    lineHeight: 19,
+    color: '#272727',
+  },
+
+  content: {
+    verticalAlign: 'top',
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#272727',
+  },
+
+  subwayLine: {
+    verticalAlign: 'top',
+    fontSize: 12,
+    lineHeight: 14,
+  },
+
+  chat: {
+    verticalAlign: 'top',
+    fontSize: 12,
+    lineHeight: 14,
   },
 })
+
+const Icon = styled(WithLocalSvg)`
+  width: 24;
+  height: 24;
+  color: ${colors.black};
+`
 
 export default LostPost
--- screens/LostFound/MainScreen.tsx
+++ screens/LostFound/MainScreen.tsx
@@ -6,8 +6,9 @@ import styled from '@emotion/native'
 import { WithLocalSvg } from 'react-native-svg'
 import { colors, icons } from '../../App/config/globalStyle'
 import CustomSwitch from './CustomSwitch'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import LostPostList from './LostPostList'
+import PopupModal from '../../components/common/PopupModal'
 
 const LostFoundMainScreen = () => {
   const handleMenuClick = () => {}
@@ -53,6 +54,31 @@ const LostFoundMainScreen = () => {
     },
     {
       id: 4,
+      title: '에어팟 맥스 발견함',
+      content: '방금 2호선 강남역에서 에어팟 맥스를 발견함. 쪽지 주셈.',
+      writer: 'kim',
+      createdBy: '2',
+      date: '2023/05/11',
+      subwayLine: 2,
+      chats: 9,
+      imgUrl: '',
+      status: 'COMPLETE',
+    },
+    {
+      id: 5,
+      title: '검정색 루이비통 지갑 발견했어요',
+      content:
+        '방금 4호선 당고개행에서 검정색 루이비통 지갑을 발견했어요. 제가 보관하고 있으니 쪽지 주세요.',
+      writer: 'seilylook',
+      createdBy: '1',
+      date: '2023/05/14',
+      subwayLine: 4,
+      chats: 2,
+      imgUrl: '',
+      status: 'PROGRESS',
+    },
+    {
+      id: 6,
       title: '에어팟 맥스 발견함',
       content: '방금 2호선 강남역에서 에어팟 맥스를 발견함. 쪽지 주셈.',
       writer: 'kim',
@@ -110,7 +136,6 @@ const LostFoundMainScreen = () => {
             bgColor="#D0EEFF"
           />
         </View>
-
         {/* 유실물 리스트 보여주기 */}
         {lostPosts && <LostPostList posts={lostPosts} />}
       </View>
