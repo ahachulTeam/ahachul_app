e3a61304d6440e8aa5a8500328c784f128fbb9fb
Suji Chae <63655826+suji8073@users.noreply.github.com>
2023-05-26 09:56:12 +0900

    feat: MainStack headerShown: false 옵션 설정

--- App/stacks/MainStack.tsx
+++ App/stacks/MainStack.tsx
@@ -1,16 +1,19 @@
 import { createStackNavigator } from '@react-navigation/stack'
 import WithAuth from '../../components/public/WithAuth'
 import TabStack from './TabStack'
+import LostFoundWriteScreen from '../../screens/LostFound/WriteScreen'
 
 const Stack = createStackNavigator()
 
 const MainStack = () => {
   return (
-    <Stack.Navigator initialRouteName="TabStack">
+    <Stack.Navigator
+      initialRouteName="TabStack"
+      screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="TabStack" component={TabStack} />
       <Stack.Screen
-        name="TabStack"
-        component={TabStack}
-        options={{ headerShown: false }}
+        name="LostFoundWriteScreen"
+        component={LostFoundWriteScreen}
       />
     </Stack.Navigator>
   )
