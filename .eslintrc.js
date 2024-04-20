2fa5f7fafecab9dbf7fbf03231535720d1f15a9b
createhb21 <alsghk9701@gmail.com>
2024-04-20 22:44:33 +0900

    [FEAT] - remove prev files

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:prettier/recommended'],
-  rules: {},
-  plugins: ['@typescript-eslint'],
-}
--- env.example.js
+++
@@ -1,4 +0,0 @@
-export default {
-  devAndroidApiUrl: 'http://192.168.0.1:3010',
-  devIosApiUrl: 'http://localhost:3010',
-}
